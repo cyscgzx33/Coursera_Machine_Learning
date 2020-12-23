2998d206830e7ec6f750d3367e7af5e47f029b95
Yusen Chen <chenyus@umich.edu>
2020-12-22 22:41:40 -0500

    feat: finished the ex1 optional part

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,16 +16,16 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    tmp = zeros(size(X, 2), 1);
+    for j = 1:size(X, 2)
+        for i = 1:m
+            tmp(j) = tmp(j) + (theta' * X(i, :)' - y(i)) * X(i, j);
+        end
+    end
+    
+    for j = 1:size(X, 2)
+        theta(j) = theta(j) - alpha / m * tmp(j);
+    end
 
     % ============================================================
 
